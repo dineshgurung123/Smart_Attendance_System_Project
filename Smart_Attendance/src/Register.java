de306034b55db2fb13b4ae6e5ec9685064f6908a
sachin magar <gitsachin123@gmail.com>
2024-07-24 18:59:23 -0800

    Face detection added to Register

--- Smart_Attendance/src/Register.java
+++ Smart_Attendance/src/Register.java
@@ -1,5 +1,10 @@
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
 import org.opencv.highgui.HighGui;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 import org.opencv.videoio.VideoCapture;
 
 import java.awt.event.ActionEvent;
@@ -12,11 +17,12 @@ public class Register {
 
     Mat matFrame;
     VideoCapture capture;
-
+    MatOfRect faces;
     Timer timer;
+    CascadeClassifier facedetector;
 
     Register() {
-
+        initializeClassifier();
         InitializeCamera();
     }
 
@@ -27,6 +33,7 @@ public class Register {
 
             capture.open(0);
         } else {
+            faces = new MatOfRect();
             startCamera();
         }
 
@@ -36,14 +43,22 @@ public class Register {
     public void startCamera() {
         matFrame = new Mat();
 
-                timer = new Timer();
+        timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
                 if (capture.isOpened()) {
                     capture.read(matFrame);
+                    facedetector.detectMultiScale(matFrame,faces);
+
+                    //draw rectangle around faces
+                    for (Rect face : faces.toArray())
+                    {
+                        Imgproc.rectangle(matFrame,new org.opencv.core.Point(face.x,face.y),new org.opencv.core.Point(face.x+face.width,face.y+face.height), new Scalar(0,255,0),2);
+                    }
 
                     if (!matFrame.empty()) {
+
                         // Display the frame using HighGui (OpenCV)
                         HighGui.imshow("Webcam Feed", matFrame);
 
@@ -53,6 +68,20 @@ public class Register {
                 }
             }
         }, 0, 30); // Initial delay of 0 milliseconds, repeat every 30 milliseconds
+    }
+
+    public void initializeClassifier()
+    {
+        facedetector = new CascadeClassifier("HAAR file/haarcascade_frontalface_default.xml");
+
+        if (facedetector.empty())
+        {
+            System.out.println("classifier not loaded");
+        }
+        else
+        {
+            System.out.println("classifier loaded");
+        }
     }
 
 }
